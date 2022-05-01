0ecf070208e726479de320a29f28020a5e53e50d
yui666a <haiso666@gmail.com>
2022-05-01 22:59:45 +0900

    add seating chart design

---
+++ front/src/components/modules/SeatingChart.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="seating-chart-section">
+    <div class="seating-chart-area">
+      <div v-for="column of 9" :key="column" class="seat-column">
+        <div
+          v-for="row of 8"
+          :key="row"
+          class="seat-row"
+          :set="(seat = '8' + ((column - 1) * 8 + row).toString().padStart(2, '0'))"
+        >
+          <SeatInfo :seatId="seat" :seat="seats[seat]"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SeatInfo from "./Seat.vue";
+
+export default {
+  name: "SeatingChart",
+  components: {
+    SeatInfo,
+  },
+  data() {
+    return {
+      // sample data
+      seats: {
+        801: {
+          pcName: "ics801",
+          studentId: "123456",
+          studentName: "まつもと ただのぶ",
+        },
+        802: {
+          pcName: "ics802",
+          studentId: "123457",
+          studentName: "あいそ",
+        },
+        803: {
+          pcName: "ics801",
+          studentId: "123456",
+          studentName: "まつもと ただのぶ",
+        },
+        804: {
+          pcName: "ics802",
+          studentId: "123457",
+          studentName: "あいそ",
+        },
+        805: {
+          pcName: "ics801",
+          studentId: "123456",
+          studentName: "まつもと ただのぶ",
+        },
+        870: {
+          pcName: "ics802",
+          studentId: "123457",
+          studentName: "あいそ",
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.seating-chart-area{
+  overflow-x: scroll;
+  overflow-y: scroll;
+}
+.seat-column {
+  display: flex;
+}
+</style>
