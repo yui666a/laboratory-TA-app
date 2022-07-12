80cafe07348f5cb2d92a470f184e1bb24920e50c
MasayaMatsushita <s183367@stn.nagaokaut.ac.jp>
2022-07-13 08:06:01 +0900

    callAPIのコード修正

--- backend/Messenger/WEB-INF/src/servlet/Call.java
+++ backend/Messenger/WEB-INF/src/servlet/Call.java
@@ -24,18 +24,19 @@ public class Call extends HttpServlet {
 		
 		//URLからpc番号を取得
 		String url = req.getRequestURI();
-		int beginIdx = url.indexOf("8"); //icsのsの位置を取得
-		String myPcId = url.substring(beginIdx+1); //実際は番号だけ知りたいので+1する
+		int beginIdx = url.lastIndexOf("/");
+		String myPcId = url.substring(beginIdx+1); //beginIdxは検索文字の位置なので+1する
+		System.out.println(myPcId);
 		
-		Pc pc = getPcFromPcId("ics8"+myPcId);
+		Pc pc = getPcFromPcId("ics"+myPcId);
 		if(pc != null) {
 			
 			//現在の挙手状態を取得
 			boolean preHandStatus = pc.getHandStatus();
 			
 			//現在の挙手状態を反転
-			if(preHandStatus) StartServlet.setHandStatus("ics8"+myPcId, false);
-			else StartServlet.setHandStatus("ics8"+myPcId, true);
+			if(preHandStatus) StartServlet.setHandStatus("ics"+myPcId, false);
+			else StartServlet.setHandStatus("ics"+myPcId, true);
 			
 			// Requestに各種データを保存
 			req.setAttribute("pcIpAddress", pc.getIpAdress());
