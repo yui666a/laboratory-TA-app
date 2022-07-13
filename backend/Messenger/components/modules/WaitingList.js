bd06a7099c19b25e56cfac22ed38a2a86f32be0b
MasayaMatsushita <s183367@stn.nagaokaut.ac.jp>
2022-07-13 15:18:52 +0900

    WatingList.jsの追加

---
+++ backend/Messenger/components/modules/WaitingList.js
@@ -0,0 +1,56 @@
+import WaitingPerson from "./WaitingPerson.js";
+import Button from "../common/Button.js";
+
+export default {
+  name: "WaitingList",
+  components: {
+    WaitingPerson,
+    Button,
+  },
+  template: `
+    <div class="waiting-list-section">
+      <div class="waiting-list-area">
+        <div v-for="waiting in waitingLi" :key="waiting.studentId">
+          <WaitingPerson :waitingPerson="waiting" />
+        </div>
+      </div>
+      <Button :value="buttonText" :onClick="onClickHandButton" />
+    </div>
+  `,
+  data() {
+    return {
+      buttonText: "手を下ろす",
+      waitingLi: [
+        {
+          pcName: "ics810",
+        },
+        {
+          pcName: "ics821",
+          studentId: "123457",
+          studentName: "ヨシダ　トミオ",
+        },
+        {
+          pcName: "ics831",
+          studentId: "123458",
+          studentName: "マツシタ　マサヤ",
+        },
+        {
+          pcName: "ics833",
+          studentId: "123459",
+          studentName: "アソウ　タロウ",
+        },
+        {
+          pcName: "ics844",
+          studentId: "123460",
+          studentName: "アイソ　ヒトシ",
+        },
+      ],
+    };
+  },
+  methods: {
+    onClickHandButton() {
+      // TODO: implement me
+      console.log("button was clicked");
+    },
+  },
+};
