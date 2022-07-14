69334f1e7f8f171720e033cef57eaf478c02de1e
yui666a <haiso666@gmail.com>
2022-07-14 14:55:14 +0900

    if文で分岐

--- backend/Messenger/components/modules/Seat.js
+++ backend/Messenger/components/modules/Seat.js
@@ -3,10 +3,15 @@ export default {
   template: `
     <div v-if="seat" class="seat-info">
       <div> ics{{ seatId }} </div>
-      <div v-if="seat.handStatus">
+      <div v-if="seat.helpStatus === 'Troubled'">
         <span> 挙手 </span>
       </div>
-      <div v-else><span>&nbsp;</span></div>
+      <div v-else-if="seat.helpStatus === 'Supporting'">
+        <span> 対応中 </span>
+      </div>
+      <div v-else>
+        <span>&nbsp;</span>
+      </div>
     </div>
 
     <div v-else class="seat-info sign-out">
